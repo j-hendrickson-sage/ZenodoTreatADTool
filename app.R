8ce3001316d265d9d15e10f34514cb28959be559
Jared Hendrickson <jared.hendrickson@sagebase.org>
2022-12-08 15:24:28 -0800

    automatic deployment

--- app.R
+++ app.R
@@ -2,8 +2,6 @@ library(DT)
 library(shiny)
 library(reticulate)
 
-reticulate::virtualenv_create(envname = 'python3_env')
-reticulate::virtualenv_install('python3_env', packages = c('requests', "pandas"))
 reticulate::use_virtualenv('python3_env', required = T)
 reticulate::source_python("requestData.py")
 
