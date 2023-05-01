21257e1a8d660705892badce7464bf5b8f6386c4
Jared Hendrickson <jared.hendrickson@sagebase.org>
2023-05-01 13:43:48 -0700

    add publication date field

--- requestData.py
+++ requestData.py
@@ -6,7 +6,7 @@ def getResponse(community):
   ACCESS_TOKEN = os.getenv("ZenodoAPI")
 
   response = requests.get('https://zenodo.org/api/records',
-                        params={'communities': community, 'size': 1000,
+                        params={'communities': community, 'size': 10000,
                                 'access_token': ACCESS_TOKEN})
   
   return response
@@ -16,7 +16,7 @@ def requestData(response, communityDisplayName):
   data = response.json()
   
   #simplify needed json response
-  neededData = data['hits']
+  neededData = data['hits']['hits']
   
   doi = []
   community = []
@@ -25,16 +25,18 @@ def requestData(response, communityDisplayName):
   uniqueDownloads = []
   views = []
   downloads = []
+  publicationDate = []
 
-  for i in range(0,  len(neededData['hits'])):
-    doi.append(neededData['hits'][i]['metadata']['doi'])
+  for i in range(0,  len(neededData)):
+    doi.append(neededData[i]['metadata']['doi'])
     community.append(communityDisplayName)
-    title.append(neededData['hits'][i]['metadata']['title'])
-    uniqueViews.append(neededData['hits'][i]['stats']['unique_views'])
-    uniqueDownloads.append(neededData['hits'][i]['stats']['unique_downloads'])
-    views.append(neededData['hits'][i]['stats']['version_views'])
-    downloads.append(neededData['hits'][i]['stats']['version_downloads'])
+    title.append(neededData[i]['metadata']['title'])
+    uniqueViews.append(neededData[i]['stats']['unique_views'])
+    uniqueDownloads.append(neededData[i]['stats']['unique_downloads'])
+    views.append(neededData[i]['stats']['version_views'])
+    downloads.append(neededData[i]['stats']['version_downloads'])
+    publicationDate.append(neededData[i]['metadata']['publication_date'])
     
-    df = pd.DataFrame(list(zip(doi, community, title, uniqueViews, uniqueDownloads, views, downloads)), columns =['doi', 'community', 'title', "uniqueViews", "uniqueDownlods", "views", "downloads"])
+    df = pd.DataFrame(list(zip(doi, community, title, uniqueViews, uniqueDownloads, views, downloads, publicationDate)), columns =['doi', 'community', 'title', "uniqueViews", "uniqueDownlods", "views", "downloads", "publicationDate"])
     
   return df
